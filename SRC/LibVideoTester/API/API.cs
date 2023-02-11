6b7239e4917643e26d2fc4387f1f7641151cd159
ShaneRH <shane@renderheads.com>
2023-02-11 12:54:48 +0200

    document the API layer

--- SRC/LibVideoTester/API/API.cs
+++ SRC/LibVideoTester/API/API.cs
@@ -11,6 +11,13 @@ namespace LibVideoTester.Api
 {
     public static class VideoTesterApi
     {
+        /// <summary>
+        /// Reads video meta data from a video file, such as width, height, bitrate, framerate and codec 
+        /// </summary>
+        /// <param name="path">The path to the file to read</param>
+        /// <param name="metaDataProvider">by Default, it will use the FFProbeMetaDataProvider, but you can create your own for custom behaviour.</param>
+        /// <param name="deserializer"><by default, will use the FFProbeMetaDataToVideoInfo class, but you can create your own for custom behaviour (for example if you mutate the incoming data somehow from standard ffprobe output)./param>
+        /// <returns>A VideoInfo object populated with the information it was able to retrieve. Don't assume that if you get an object, that the readings are valid though.</returns>
         public static async Task<VideoMetaData> GetVideoMetaDataAsync(string path, IVideoMetaDataProvider metaDataProvider = null, IDeserializer<VideoMetaData> deserializer = null)
         {
             IVideoMetaDataProvider providerToUse = metaDataProvider == null ? new FFProbeMetaDataProvider() : metaDataProvider;
@@ -19,6 +26,13 @@ namespace LibVideoTester.Api
             return await videoInfoGenerator.GetVideoInfoAsync(path);
         }
 
+        /// <summary>
+        /// Get all configuration files for in a specified directory, by default it will use the "Configurations" directory.
+        /// </summary>
+        /// <param name="directoryToCheck">The path to check, by default it will only look for .json files.</param>
+        /// <param name="fileProvider">By default will use JsonFileProvider, but you can create your own if you want to serve files from non disk locations.</param>
+        /// <param name="deserializer">By default will use NewtonSoftJsonDeserializer, but you can create your own for custom behaviours.</param>
+        /// <returns>A dictionary of configurations, where the key is the path to the file containing the config, and the value is the Configuration object generated from it.</returns>
         public static async Task<Dictionary<string, Configuration>> GetConfigurationsAsync(string directoryToCheck = "Configurations", IFileProvider fileProvider = null, IDeserializer<Configuration> deserializer = null)
         {
             IFileProvider fileProviderToUse = fileProvider == null ? new JsonFileProvider() : fileProvider;
@@ -28,6 +42,13 @@ namespace LibVideoTester.Api
             return await configurationFactory.GetConfigurations();
         }
 
+
+        /// <summary>
+        /// Use this to compare your metadata against configurations.
+        /// </summary>
+        /// <param name="data">The video metadata to check</param>
+        /// <param name="configurations">The configurations to check against</param>
+        /// <returns>A dictionary of all configurations that matched the video</returns>
         public static Dictionary<string, Configuration> FindMatches(VideoMetaData data, Dictionary<string, Configuration> configurations)
         {
             Dictionary<string, Configuration> matches = new Dictionary<string, Configuration>();
@@ -41,7 +62,13 @@ namespace LibVideoTester.Api
             return matches;
         }
 
-
+        /// <summary>
+        /// A easy to use helper method to that works as a oneline to get all matching configurations for a given video
+        /// </summary>
+        /// <param name="pathToVideo">The path to the video to check</param>
+        /// <param name="metaDataProvider">by Default, it will use the FFProbeMetaDataProvider, but you can create your own for custom behaviour.</param>
+        /// <param name="fileProvider">By default will use JsonFileProvider, but you can create your own if you want to serve files from non disk locations.</param>
+        /// <returns>A dictionary of all configurations that matched the video</returns>
         public static async Task<Dictionary<string, Configuration>> ExtractMetaDataAndFindConfigMatchesAsync(string pathToVideo, IVideoMetaDataProvider metaDataProvider = null, IFileProvider fileProvider = null)
         {
             IVideoMetaDataProvider metaDataProviderToUse = metaDataProvider == null ? new FFProbeMetaDataProvider() : metaDataProvider;
