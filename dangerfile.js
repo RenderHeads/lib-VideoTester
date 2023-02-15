3e680244b48a8fc8f5599dd713d22f17ab9dacc8
ruan-renderheads <125180034+ruan-renderheads@users.noreply.github.com>
2023-02-15 15:36:05 +0200

    Add Danger to repo (#6)
    The PR adds a GitHub action to run *Danger JS* on pull requests.
    Currently there are only three checks:
    1. The PR title starts with a capital letter and is shorter than 80
    characters.
    2. The PR has an assignee.
    3. The PR has a description.

---
+++ dangerfile.js
@@ -0,0 +1,21 @@
+const {danger, warn} = require('danger')
+
+
+// Ensure the title is in the correct format
+let title = danger.github.pr.title;
+if (title[0] !== title[0].toUpperCase()) {
+  fail("PR title needs to start with an uppercase letter.")
+}
+if (title.length > 80) {
+  fail("PR title is too long (max 80 characters).")
+}
+
+// Every PR needs someone assigned to it
+if (!danger.github.pr.assignee) {
+  fail("This pull request needs an assignee, and optionally include any reviewers.")
+}
+
+// No PR is too small to include a description of why you made a change
+if (danger.github.pr.body.length < 10) {
+  warn('Please include a description of your PR changes.');
+}
