6550d0697183d97839c69489afb282c1291061ec
ShaneRH <shane@renderheads.com>
2023-02-15 23:24:49 +0200

    Adds the ability to find out what is wrong with resolution checks (#7)
    - Fixes a bug where namespace was represented in c# 8.0 style, not
    currently supported
    - Adds unit tests and a feature extension which returns the reasons for
    the resolution check failing, so we can provide more useful feedback to
    users.

--- SRC/LibVideoTester/Providers/FFProbeMetaDataProvider.cs
+++ SRC/LibVideoTester/Providers/FFProbeMetaDataProvider.cs
@@ -2,31 +2,33 @@
 using System.Diagnostics;
 using System.Threading.Tasks;
 
-namespace LibVideoTester.Providers;
-
-//NOTE THIS CAN"T BE UNIT TESTED AS IT TESTS SOMETHING OUTSIDE OF OF THE PROJECT    
-public class FFProbeMetaDataProvider : IVideoMetaDataProvider
+namespace LibVideoTester.Providers
 {
-    private string _executablePath;
-    public FFProbeMetaDataProvider(string executablePath = "ffprobe") {
-        _executablePath = executablePath;
-    }
-
-    public async Task<string> GetMetaDataFromFile(string filename)
+    //NOTE THIS CAN"T BE UNIT TESTED AS IT TESTS SOMETHING OUTSIDE OF OF THE PROJECT    
+    public class FFProbeMetaDataProvider : IVideoMetaDataProvider
     {
-        var process = new Process
+        private string _executablePath;
+        public FFProbeMetaDataProvider(string executablePath = "ffprobe")
         {
-            StartInfo = { FileName = _executablePath,
+            _executablePath = executablePath;
+        }
+
+        public async Task<string> GetMetaDataFromFile(string filename)
+        {
+            var process = new Process
+            {
+                StartInfo = { FileName = _executablePath,
             Arguments = $"-v error -select_streams v:0 -show_entries stream=width,height,duration,bit_rate,r_frame_rate,codec_name -of default=noprint_wrappers=1 \"{filename}\"",
             UseShellExecute = false,
             RedirectStandardOutput = true,
             CreateNoWindow = true
             }
-        };
-        await Task.Run(() => process.Start());
-        string result =  await process.StandardOutput.ReadToEndAsync();
-        return result;
+            };
+            await Task.Run(() => process.Start());
+            string result = await process.StandardOutput.ReadToEndAsync();
+            return result;
 
+        }
     }
-}
 
+}
