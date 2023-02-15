2af53e126ac542bac9795b852307088fd01d704b
ShaneRH <shane@renderheads.com>
2023-02-15 08:17:29 +0200

    simplifies ffprobe provider usage
    I exposed the path to ffprobe in constructor so we can have a single provider, instead of another one for mac.  I will also change the path whether device is mac or windows

--- SRC/LibVideoTester/Providers/FFProbeMetaDataProvider.cs
+++ SRC/LibVideoTester/Providers/FFProbeMetaDataProvider.cs
@@ -1,32 +1,32 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
-using LibVideoTester.Providers;
 
-namespace LibVideoTester
+namespace LibVideoTester.Providers;
+
+//NOTE THIS CAN"T BE UNIT TESTED AS IT TESTS SOMETHING OUTSIDE OF OF THE PROJECT    
+public class FFProbeMetaDataProvider : IVideoMetaDataProvider
 {
-    //NOTE THIS CAN"T BE UNIT TESTED AS IT TESTS SOMETHING OUTSIDE OF OF THE PROJECT    
-    public class FFProbeMetaDataProvider : IVideoMetaDataProvider
+    private string _executablePath;
+    public FFProbeMetaDataProvider(string executablePath = "ffprobe") {
+        _executablePath = executablePath;
+    }
+
+    public async Task<string> GetMetaDataFromFile(string filename)
     {
-        public const string EXECUTABLE_NAME = "ffprobe";
-
- 
-        public async Task<string> GetMetaDataFromFile(string filename)
+        var process = new Process
         {
-            var process = new Process
-            {
-                StartInfo = { FileName = EXECUTABLE_NAME,
-                Arguments = $"-v error -select_streams v:0 -show_entries stream=width,height,duration,bit_rate,r_frame_rate,codec_name -of default=noprint_wrappers=1 \"{filename}\"",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true
-                }
-            };
-            await Task.Run(() => process.Start());
-            string result =  await process.StandardOutput.ReadToEndAsync();
-            return result;
+            StartInfo = { FileName = _executablePath,
+            Arguments = $"-v error -select_streams v:0 -show_entries stream=width,height,duration,bit_rate,r_frame_rate,codec_name -of default=noprint_wrappers=1 \"{filename}\"",
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true
+            }
+        };
+        await Task.Run(() => process.Start());
+        string result =  await process.StandardOutput.ReadToEndAsync();
+        return result;
 
-        }
     }
 }
 
--- SRC/VideoTesterApp/MainPage.xaml.cs
+++ SRC/VideoTesterApp/MainPage.xaml.cs
@@ -1,38 +1,41 @@
 ﻿using LibVideoTester.Api;
 using LibVideoTester.Models;
-
+using LibVideoTester.Providers;
 namespace VideoTesterApp;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    private const string FFPROBE_PATH_MAC = "./Contents/Resources/ExternalBinaries/Mac/ffprobe";
+    private const string FFPROBE_PATH_WINDOWS = "ffprobe"; //assume it is just next to executable?
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
         GetFile();
-	}
+    }
 
-	public async Task GetFile()
-	{
+    public async Task GetFile()
+    {
         try
         {
-            ResultsLabel.TextColor = Color.FromRgb(255, 255,255);
+            ResultsLabel.TextColor = Color.FromRgb(255, 255, 255);
             CounterBtn.IsEnabled = false;
             var result = await FilePicker.Default.PickAsync();
             if (result != null)
             {
                 CounterBtn.Text = "Please wait processing...";
-                VideoMetaData v = await VideoTesterApi.GetVideoMetaDataAsync(result.FullPath, new FFProbeMetaDataProviderMacApp());
+                VideoMetaData v = await VideoTesterApi.GetVideoMetaDataAsync(result.FullPath, new FFProbeMetaDataProvider(DeviceInfo.Current.Platform == DevicePlatform.macOS ? FFPROBE_PATH_MAC : FFPROBE_PATH_WINDOWS));
                 Dictionary<string, Configuration> configs = await VideoTesterApi.GetConfigurationsAsync("./Contents/Resources/Configurations");
 
                 VideoMetaDataLabel.Text = $"Width: {v.Width} Height: {v.Height} Codec: {v.Codec} Framerate: {v.FrameRate} Bitrate (KPBS): {v.BitrateKPBS}";
                 var matches = VideoTesterApi.FindMatches(v, configs);
                 if (matches.Keys.Count == 0)
                 {
-                    ResultsLabel.Text =  $"Out of {configs.Keys.Count} configurations found, No configuration match, this is a bad encode! do not use.";
+                    ResultsLabel.Text = $"Out of {configs.Keys.Count} configurations found, No configuration match, this is a bad encode! do not use.";
                     ResultsLabel.TextColor = Color.FromRgb(255, 0, 0);
                 }
                 else
@@ -47,7 +50,7 @@ public partial class MainPage : ContentPage
                 CounterBtn.Text = "Something went wrong selecting a file, please try again";
 
             }
-             
+
         }
         catch (Exception ex)
         {
